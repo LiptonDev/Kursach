3f8f4c2b695b2acbc41c4a396122f888d94cb500
Alexander <zam.kuratora@gmail.com>
2019-12-07 21:22:43 +0300

    added main vm & login vm & validate vm

---
+++ Kursach/ViewModels/LoginViewModel.cs
@@ -0,0 +1,60 @@
+﻿using DevExpress.Mvvm;
+using Kursach.Models;
+using MaterialDesignXaml.DialogsHelper;
+using MaterialDesignXaml.DialogsHelper.Enums;
+using Prism.Regions;
+using System.Windows.Input;
+
+namespace Kursach.ViewModels
+{
+    /// <summary>
+    /// Login view model.
+    /// </summary>
+    class LoginViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Пользователь для авторизации.
+        /// </summary>
+        public LoginUser User { get; }
+
+        /// <summary>
+        /// Менеджер регионов.
+        /// </summary>
+        readonly IRegionManager regionManager;
+
+        /// <summary>
+        /// Идентификатор диалоговых окон.
+        /// </summary>
+        readonly IDialogIdentifier dialogIdentifier;
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        public LoginViewModel(IRegionManager regionManager, IDialogIdentifier dialogIdentifier)
+        {
+            this.regionManager = regionManager;
+            this.dialogIdentifier = dialogIdentifier;
+
+            TryLoginCommand = new DelegateCommand(TryLogin);
+
+            User = new LoginUser();
+        }
+
+        /// <summary>
+        /// Команда попытки входа в систему.
+        /// </summary>
+        public ICommand TryLoginCommand { get; }
+
+        /// <summary>
+        /// Попытка входа в систему.
+        /// </summary>
+        private void TryLogin()
+        {
+            if (!User.IsValid)
+            {
+                dialogIdentifier.ShowMessageBoxAsync(User.Error, MaterialMessageBoxButtons.Ok);
+                return;
+            }
+        }
+    }
+}
---
+++ Kursach/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using DevExpress.Mvvm;
+using Kursach.Models;
+using MaterialDesignXaml.DialogsHelper;
+using Prism.Regions;
+using System.Windows.Input;
+
+namespace Kursach.ViewModels
+{
+    /// <summary>
+    /// MainWindow view model.
+    /// </summary>
+    class MainWindowViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Настройки программы.
+        /// </summary>
+        public IProgramSettings Settings { get; }
+
+        /// <summary>
+        /// Идентификатор диалоговых окон.
+        /// </summary>
+        public IDialogIdentifier DialogIdentifier { get; }
+
+        /// <summary>
+        /// Менеджер регионов.
+        /// </summary>
+        readonly IRegionManager regionManager;
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        public MainWindowViewModel(IProgramSettings settings, IRegionManager regionManager, IDialogIdentifier dialogIdentifier)
+        {
+            Settings = settings;
+            this.regionManager = regionManager;
+            DialogIdentifier = dialogIdentifier;
+
+            LoadCommand = new DelegateCommand(OnLoad);
+            CloseCommand = new DelegateCommand(OnClose);
+        }
+
+        /// <summary>
+        /// Команда открытия программы.
+        /// </summary>
+        public ICommand LoadCommand { get; }
+
+        /// <summary>
+        /// Команда закрытия программы.
+        /// </summary>
+        public ICommand CloseCommand { get; }
+
+        /// <summary>
+        /// Вызывается при закрытии окна программы.
+        /// </summary>
+        private void OnClose()
+        {
+            ProgramSettings.Save(Settings);
+
+            Logger.Log.Info("Закрытие программы");
+        }
+
+        /// <summary>
+        /// Вызывается при открытии окна программы.
+        /// </summary>
+        private void OnLoad()
+        {
+            regionManager.RequestNavigateInRootRegion(RegionViews.LoginView);
+
+            Logger.Log.Info("Программа открыта");
+        }
+    }
+}
