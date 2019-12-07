42919edfdb2e17ce7fb41d89020cb302def4f498
Alexander <zam.kuratora@gmail.com>
2019-12-08 02:31:25 +0300

    added data base context

---
+++ Kursach/DataBase/Context.cs
@@ -0,0 +1,15 @@
+﻿using Kursach.Models;
+using System.Data.Entity;
+
+namespace Kursach.DataBase
+{
+    class Context : DbContext
+    {
+        public Context() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<SignInLog> SignInLogs { get; set; }
+    }
+}
