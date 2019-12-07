a4092a21edbde16dc2eca28f6c152677767c33bc
Alexander <zam.kuratora@gmail.com>
2019-12-08 02:31:15 +0300

    added new converters

---
+++ Kursach/Converters/ActualSizeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Kursach.Converters
+{
+
+    [ValueConversion(typeof(double), typeof(double))]
+    class ActualSizeConverter : IValueConverter
+    {
+        public double Add { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double s = (double)value;
+
+            return s + Add;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
