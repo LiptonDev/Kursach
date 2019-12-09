c3f55536169c1ab08340590ead9aaf793acb046d
Alexander <zam.kuratora@gmail.com>
2019-12-10 00:11:46 +0300

    added staffview & staffeditor

---
+++ Kursach/ViewModels/StaffEditorViewModel.cs
@@ -0,0 +1,78 @@
+﻿using DevExpress.Mvvm;
+using DryIoc;
+using Kursach.DataBase;
+using Kursach.Dialogs;
+using Kursach.Models;
+using MaterialDesignXaml.DialogsHelper;
+using MaterialDesignXaml.DialogsHelper.Enums;
+using System.Windows.Input;
+
+namespace Kursach.ViewModels
+{
+    /// <summary>
+    /// Staff editor view model.
+    /// </summary>
+    [DialogName(nameof(Views.StaffEditorView))]
+    class StaffEditorViewModel : IClosableDialog, IDialogIdentifier, IEditMode
+    {
+        /// <summary>
+        /// Identifier.
+        /// </summary>
+        public string Identifier => "StaffEditorViewModel";
+
+        /// <summary>
+        /// Owner.
+        /// </summary>
+        public IDialogIdentifier OwnerIdentifier { get; }
+
+        /// <summary>
+        /// Менеджер диалогов.
+        /// </summary>
+        readonly IDialogManager dialogManager;
+
+        /// <summary>
+        /// True - Режим редактирования группы, иначе - добавление.
+        /// </summary>
+        public bool IsEditMode { get; }
+
+        /// <summary>
+        /// Сотрудник.
+        /// </summary>
+        public Staff Staff { get; }
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        public StaffEditorViewModel(Staff staff, bool isEditMode, IContainer container, IDialogManager dialogManager)
+        {
+            IsEditMode = isEditMode;
+            OwnerIdentifier = container.ResolveRootDialogIdentifier();
+            this.dialogManager = dialogManager;
+
+            if (isEditMode)
+                Staff = (Staff)staff.Clone();
+            else Staff = new Staff();
+
+            CloseDialogCommand = new DelegateCommand(CloseDialog);
+        }
+
+        /// <summary>
+        /// Команда закрытия диалога.
+        /// </summary>
+        public ICommand CloseDialogCommand { get; }
+
+        /// <summary>
+        /// Закрытие диалога.
+        /// </summary>
+        private async void CloseDialog()
+        {
+            if (!Staff.IsValid)
+            {
+                await this.ShowMessageBoxAsync(Staff.Error, MaterialMessageBoxButtons.Ok);
+                return;
+            }
+
+            OwnerIdentifier.Close(Staff);
+        }
+    }
+}
---
+++ Kursach/ViewModels/StaffViewModel.cs
@@ -0,0 +1,155 @@
+﻿using DevExpress.Mvvm;
+using DryIoc;
+using Kursach.DataBase;
+using Kursach.Dialogs;
+using MaterialDesignXaml.DialogsHelper;
+using MaterialDesignXaml.DialogsHelper.Enums;
+using Prism.Regions;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Kursach.ViewModels
+{
+    /// <summary>
+    /// Staf view model.
+    /// </summary>
+    class StaffViewModel : NavigationViewModel
+    {
+        /// <summary>
+        /// Сотрудники.
+        /// </summary>
+        public ObservableCollection<Staff> Staff { get; }
+
+        /// <summary>
+        /// Пользователь.
+        /// </summary>
+        public User User { get; private set; }
+
+        /// <summary>
+        /// База данных.
+        /// </summary>
+        readonly IDataBase dataBase;
+
+        /// <summary>
+        /// Идентификатор диалогов.
+        /// </summary>
+        readonly IDialogIdentifier dialogIdentifier;
+
+        /// <summary>
+        /// Менеджер диалогов.
+        /// </summary>
+        readonly IDialogManager dialogManager;
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        public StaffViewModel(IDataBase dataBase, IContainer container, IDialogManager dialogManager)
+        {
+            this.dataBase = dataBase;
+            this.dialogIdentifier = container.ResolveRootDialogIdentifier();
+            this.dialogManager = dialogManager;
+
+            Staff = new ObservableCollection<Staff>();
+
+            EditStaffCommand = new DelegateCommand<Staff>(EditStaff);
+            DeleteStaffCommand = new DelegateCommand<Staff>(DeleteStaff);
+            AddStaffCommand = new DelegateCommand(AddStaff);
+        }
+
+        /// <summary>
+        /// Команда добавления сотрудника.
+        /// </summary>
+        public ICommand AddStaffCommand { get; }
+
+        /// <summary>
+        /// Команда редактирования сотрудника.
+        /// </summary>
+        public ICommand<Staff> EditStaffCommand { get; }
+
+        /// <summary>
+        /// Команда удаления сотрудника.
+        /// </summary>
+        public ICommand<Staff> DeleteStaffCommand { get; }
+
+        /// <summary>
+        /// Добавить сотрудника.
+        /// </summary>
+        private async void AddStaff()
+        {
+            var editor = await dialogManager.StaffEditor(null, false);
+
+            if (editor == null)
+                return;
+
+            var res = await dataBase.AddStaffAsync(editor);
+            var msg = res ? "Сотрудник добавлен" : "Сотрудник не добавлен";
+
+            Logger.Log.Info($"{msg}: {{{editor}}}");
+
+            if (res)
+                Staff.Add(editor);
+
+            await dialogIdentifier.ShowMessageBoxAsync(msg, MaterialMessageBoxButtons.Ok);
+        }
+
+        /// <summary>
+        /// Удаление сотрудника.
+        /// </summary>
+        private async void DeleteStaff(Staff staff)
+        {
+            var answ = await dialogIdentifier.ShowMessageBoxAsync($"Удалить '{staff}'?", MaterialMessageBoxButtons.YesNo);
+            if (answ != MaterialMessageBoxButtons.Yes)
+                return;
+
+            var res = await dataBase.RemoveStaffAsync(staff);
+            var msg = res ? "Сотрудник удален" : "Сотрудник не удален";
+
+            Logger.Log.Info($"{msg}: {{{staff}}}");
+
+            if (res)
+                Staff.Remove(staff);
+
+            await dialogIdentifier.ShowMessageBoxAsync(msg, MaterialMessageBoxButtons.Ok);
+        }
+
+        /// <summary>
+        /// Редактирование сотрудника.
+        /// </summary>
+        private async void EditStaff(Staff staff)
+        {
+            var editor = await dialogManager.StaffEditor(staff, true);
+
+            if (editor == null)
+                return;
+
+            staff.FirstName = editor.FirstName;
+            staff.LastName = editor.LastName;
+            staff.MiddleName = editor.MiddleName;
+            staff.Position = editor.Position;
+            var res = await dataBase.SaveStaffAsync(staff);
+            var msg = res ? "Сотрудник сохранен" : "Сотрудник не сохранен";
+
+            Logger.Log.Info($"{msg}: {{{staff}}}");
+
+            await dialogIdentifier.ShowMessageBoxAsync(msg, MaterialMessageBoxButtons.Ok);
+        }
+
+        /// <summary>
+        /// Загрузка данных.
+        /// </summary>
+        private async void Load()
+        {
+            Staff.Clear();
+            var res = await dataBase.GetStaffsAsync();
+            Staff.AddRange(res);
+        }
+
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            User = navigationContext.Parameters["user"] as User;
+
+            Load();
+        }
+    }
+}
