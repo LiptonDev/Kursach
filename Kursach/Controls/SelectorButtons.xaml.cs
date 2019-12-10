d0670fb699783fdfdb9a8aae728ac7441cf51276
Alexander <zam.kuratora@gmail.com>
2019-12-10 04:22:50 +0300

    added new control for dialogs

---
+++ Kursach/Controls/SelectorButtons.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls.Primitives;
+
+namespace Kursach.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для SelectorButtons.xaml
+    /// </summary>
+    public partial class SelectorButtons : UniformGrid
+    {
+        public SelectorButtons()
+        {
+            InitializeComponent();
+        }
+    }
+}
