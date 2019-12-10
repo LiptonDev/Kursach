032cdaf2ec96629f281894a9044c30b23c965bc4
Alexander <zam.kuratora@gmail.com>
2019-12-10 04:24:44 +0300

    added signUpResult class

---
+++ Kursach/Models/SignUpResult.cs
@@ -0,0 +1,29 @@
+﻿using Kursach.DataBase;
+
+namespace Kursach.Models
+{
+    /// <summary>
+    /// Результат регистрации.
+    /// </summary>
+    class SignUpResult
+    {
+        /// <summary>
+        /// Пользователь.
+        /// </summary>
+        public LoginUser User { get; }
+
+        /// <summary>
+        /// Права.
+        /// </summary>
+        public UserMode Mode { get; }
+
+        /// <summary>
+        /// Ctor.
+        /// </summary>
+        public SignUpResult(LoginUser user, UserMode mode)
+        {
+            User = user;
+            Mode = mode;
+        }
+    }
+}
