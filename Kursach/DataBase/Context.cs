4b0eebe0e0e79f058ce6505f4d61edb1dcb59bd0
Alexander <zam.kuratora@gmail.com>
2019-12-10 04:23:58 +0300

    fix modelBuilder

--- Kursach/DataBase/Context.cs
+++ Kursach/DataBase/Context.cs
@@ -18,11 +18,10 @@ namespace Kursach.DataBase
         {
             base.OnModelCreating(modelBuilder);
 
-            //Students => Group
-            modelBuilder.Entity<Student>().HasRequired(x => x.Group).WithMany(x => x.Students).HasForeignKey(x => x.GroupId).WillCascadeOnDelete(true);
-
-            //Group => Curator
+            modelBuilder.Entity<Group>().HasMany(x => x.Students).WithRequired(x => x.Group).HasForeignKey(x => x.GroupId).WillCascadeOnDelete(true);
             modelBuilder.Entity<Group>().HasRequired(x => x.Curator).WithMany(x => x.Groups).HasForeignKey(x => x.CuratorId).WillCascadeOnDelete(true);
+            modelBuilder.Entity<Student>().HasRequired(x => x.Group).WithMany(x => x.Students).HasForeignKey(x => x.GroupId).WillCascadeOnDelete(true);
+            modelBuilder.Entity<Staff>().HasMany(x => x.Groups).WithRequired(x => x.Curator).HasForeignKey(x => x.CuratorId).WillCascadeOnDelete(true);
         }
 
         /// <summary>
