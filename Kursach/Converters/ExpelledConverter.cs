d52a7cf2b8c58444184173d598ab676d2c149d59
Alexander <zam.kuratora@gmail.com>
2020-01-24 01:32:38 +0300

    new converters

---
+++ Kursach/Converters/ExpelledConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Kursach.Converters
+{
+    [ValueConversion(typeof(bool), typeof(string))]
+    class ExpelledConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool bv = (bool)value;
+
+            return bv ? "Студент исключен" : "Студент не отчислен";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
