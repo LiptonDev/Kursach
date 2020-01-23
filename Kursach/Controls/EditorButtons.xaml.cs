99758ac0efb6974ea1364de55a84e8aba9d4e1a7
Alexander <zam.kuratora@gmail.com>
2020-01-24 01:32:32 +0300

    new controls

---
+++ Kursach/Controls/EditorButtons.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls.Primitives;
+
+namespace Kursach.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для EditorButtons.xaml
+    /// </summary>
+    public partial class EditorButtons : UniformGrid
+    {
+        public EditorButtons()
+        {
+            InitializeComponent();
+        }
+    }
+}
