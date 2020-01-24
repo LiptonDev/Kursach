702f808dfd0915e151721a2b61f8809c5a6d6e57
Alexander <zam.kuratora@gmail.com>
2020-01-24 14:50:52 +0300

    base importer & exporter

---
+++ Kursach/Excel/BaseExporter.cs
@@ -0,0 +1,39 @@
+﻿using Kursach.Dialogs;
+
+namespace Kursach.Excel
+{
+    class BaseExporter
+    {
+        /// <summary>
+        /// Менеджер диалогов.
+        /// </summary>
+        readonly IDialogManager dialogManager;
+
+        /// <summary>
+        /// Имя файла.
+        /// </summary>
+        public string FileName { get; private set; }
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public BaseExporter(IDialogManager dialogManager)
+        {
+            this.dialogManager = dialogManager;
+        }
+
+        /// <summary>
+        /// Выбор файла для экспорта.
+        /// </summary>
+        /// <param name="defName">Начальное название файла.</param>
+        /// <returns></returns>
+        public bool SelectFile(string defName)
+        {
+            string fileName = dialogManager.SelectExportFileName(defName);
+
+            FileName = fileName;
+
+            return fileName != null;
+        }
+    }
+}
---
+++ Kursach/Excel/BaseImporter.cs
@@ -0,0 +1,38 @@
+﻿using Kursach.Dialogs;
+
+namespace Kursach.Excel
+{
+    class BaseImporter
+    {
+        /// <summary>
+        /// Менеджер диалогов.
+        /// </summary>
+        readonly IDialogManager dialogManager;
+
+        /// <summary>
+        /// Имя файла.
+        /// </summary>
+        public string FileName { get; private set; }
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public BaseImporter(IDialogManager dialogManager)
+        {
+            this.dialogManager = dialogManager;
+        }
+
+        /// <summary>
+        /// Выбор файла.
+        /// </summary>
+        /// <returns></returns>
+        public bool SelectFile()
+        {
+            string fileName = dialogManager.SelectImportFile();
+
+            FileName = fileName;
+
+            return fileName != null;
+        }
+    }
+}
