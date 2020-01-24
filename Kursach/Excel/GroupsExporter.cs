e6171f7cb0f9d50a679c6e9a0404eec4a1458b65
Alexander <zam.kuratora@gmail.com>
2020-01-24 14:52:37 +0300

    groups export

---
+++ Kursach/Excel/GroupsExporter.cs
@@ -0,0 +1,78 @@
+﻿using Kursach.DataBase;
+using Kursach.Dialogs;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Kursach.Excel
+{
+    /// <summary>
+    /// Экспорт данных о всех группах колледжа.
+    /// </summary>
+    class GroupsExporter : BaseExporter, IExporter<IEnumerable<Group>>
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public GroupsExporter(IDialogManager dialogManager) : base(dialogManager)
+        {
+
+        }
+
+        /// <summary>
+        /// Экспорт данных о всех группах колледжа.
+        /// </summary>
+        /// <param name="groups">Группы.</param>
+        public void Export(IEnumerable<Group> groups)
+        {
+            if (!SelectFile("контингент по подразделениям"))
+                return;
+
+            using (var excel = new ExcelPackage())
+            {
+                ExcelWorksheet worksheet = excel.Workbook.Worksheets.Add(DateTime.Now.ToString("MMMM"));
+
+                //Глобальный стиль.
+                worksheet.Cells.SetFontName("Arial").SetFontSize(12);
+
+                worksheet.Cells["B1"]
+                    .SetValue("Контингент обучающихся ГБПОУ БГК 2019-2020 уч.год")
+                    .SetFontSize(14)
+                    .SetBold();
+
+                worksheet.Cells["A2:F2"]
+                    .SetFontSize(10)
+                    .SetHorizontalAligment(ExcelHorizontalAlignment.Center)
+                    .SetVerticalAligment(ExcelVerticalAlignment.Center);
+
+                worksheet.Cells["E2:F2"].SetFontSize(8);
+
+                worksheet.Cells["A2:B2"].SetMerge().SetValue("Группа");
+                worksheet.Cells["C2"].SetValue("СПО/НПО").SetWrapText();
+                worksheet.Cells["D2"].SetValue("Б/К");
+                worksheet.Cells["E2"].SetValue($"На {DateTime.Now.ToString("dd.MM.yyyy")}").SetWrapText();
+                worksheet.Cells["F2"].SetValue("ак. отп").SetWrapText();
+
+                int i = 0;
+                foreach (var item in groups)
+                {
+
+                }
+
+                worksheet.Cells.AutoFitColumns(1);
+
+                try
+                {
+                    excel.SaveAs(new FileInfo(FileName));
+                    Logger.Log.Info($"Экспорт информации о группах");
+                }
+                catch (Exception ex)
+                {
+                    Logger.Log.Error($"Ошибка экспорта информации о группах: {{{ex.Message}}}");
+                }
+            }
+        }
+    }
+}
