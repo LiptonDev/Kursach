0efe8836d8f20c7c3b6043c5dd86cf79f39d59af
Alexander <zam.kuratora@gmail.com>
2020-01-24 14:52:26 +0300

    importer

---
+++ Kursach/Excel/IImporter.cs
@@ -0,0 +1,14 @@
+﻿namespace Kursach.Excel
+{
+    /// <summary>
+    /// Импорт данных.
+    /// </summary>
+    interface IImporter<T, TTarget>
+    {
+        /// <summary>
+        /// Импорт данных из Excel файла.
+        /// </summary>
+        /// <returns></returns>
+        T Import(TTarget target);
+    }
+}
