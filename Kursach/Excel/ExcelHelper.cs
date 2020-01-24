ea02b3ce5eaa977953ab72c91691d30117207162
Alexander <zam.kuratora@gmail.com>
2020-01-24 14:50:59 +0300

    added new method

--- Kursach/Excel/ExcelHelper.cs
+++ Kursach/Excel/ExcelHelper.cs
@@ -5,9 +5,26 @@ namespace Kursach.Excel
 {
     static class ExcelHelper
     {
+        public static ExcelRange SetTable(this ExcelRange excelRange)
+        {
+            excelRange.Style.Border.Top.Style = ExcelBorderStyle.Medium;
+            excelRange.Style.Border.Right.Style = ExcelBorderStyle.Medium;
+            excelRange.Style.Border.Bottom.Style = ExcelBorderStyle.Medium;
+            excelRange.Style.Border.Left.Style = ExcelBorderStyle.Medium;
+
+            return excelRange;
+        }
+
         public static ExcelRange SetWrapText(this ExcelRange excelRange, bool wrapText = true)
         {
             excelRange.Style.WrapText = wrapText;
+
+            return excelRange;
+        }
+
+        public static ExcelRange SetFontName(this ExcelRange excelRange, string name)
+        {
+            excelRange.Style.Font.Name = name;
 
             return excelRange;
         }
