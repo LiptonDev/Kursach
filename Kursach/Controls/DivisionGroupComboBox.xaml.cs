71566cc238b37c5da6dfc6f8a742c5f7d982e9b0
Alexander <zam.kuratora@gmail.com>
2020-01-27 06:45:00 +0300

    added new controls

---
+++ Kursach/Controls/DivisionGroupComboBox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Kursach.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для DivisionGroupComboBox.xaml
+    /// </summary>
+    public partial class DivisionGroupComboBox : ComboBox
+    {
+        public DivisionGroupComboBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Kursach/Controls/ImportFromExcelButton.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Kursach.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для ImportFromExcelButton.xaml
+    /// </summary>
+    public partial class ImportFromExcelButton : Button
+    {
+        public ImportFromExcelButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
