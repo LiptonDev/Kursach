262a3423218b1019de4ac4b21bc88eed8669fda1
Alexander <zam.kuratora@gmail.com>
2020-01-27 06:41:02 +0300

    excel importer

---
+++ Kursach/ViewModels/IExcelImporterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Input;
+
+namespace Kursach.ViewModels
+{
+    /// <summary>
+    /// Указывает, что ViewModel можешь импортировать данные из Excel.
+    /// </summary>
+    interface IExcelImporterViewModel
+    {
+        /// <summary>
+        /// Команда экспорта.
+        /// </summary>
+        ICommand ImportFromExcelCommand { get; }
+
+        /// <summary>
+        /// Метод экспорта.
+        /// </summary>
+        void ImportFromExcel();
+    }
+}
