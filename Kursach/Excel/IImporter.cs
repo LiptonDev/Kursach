1032970b94bf107851bb23364ba49d0ecc3894eb
Alexander <zam.kuratora@gmail.com>
2020-01-27 06:43:56 +0300

    added async importer

--- Kursach/Excel/IImporter.cs
+++ Kursach/Excel/IImporter.cs
@@ -1,14 +1,40 @@
-﻿namespace Kursach.Excel
+﻿using System.Threading.Tasks;
+
+namespace Kursach.Excel
 {
     /// <summary>
     /// Импорт данных.
     /// </summary>
-    interface IImporter<T, TTarget>
+    interface IAsyncImporter<T>
     {
         /// <summary>
         /// Импорт данных из Excel файла.
         /// </summary>
         /// <returns></returns>
-        T Import(TTarget target);
+        Task<T> Import();
+    }
+
+    /// <summary>
+    /// Импорт данных.
+    /// </summary>
+    interface IAsyncImporter<T, TArg>
+    {
+        /// <summary>
+        /// Импорт данных из Excel файла.
+        /// </summary>
+        /// <returns></returns>
+        Task<T> Import(TArg arg);
+    }
+
+    /// <summary>
+    /// Импорт данных.
+    /// </summary>
+    interface IImporter<T>
+    {
+        /// <summary>
+        /// Импорт данных из Excel файла.
+        /// </summary>
+        /// <returns></returns>
+        T Import();
     }
 }
