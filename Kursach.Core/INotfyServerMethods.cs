92ffc2b7d0ee96a3329251933f229334b9478f2c
Alexander <zam.kuratora@gmail.com>
2020-01-31 00:00:10 +0300

    initial commit

---
+++ Kursach.Core/INotfyServerMethods.cs
@@ -0,0 +1,39 @@
+﻿namespace Kursach.Core
+{
+    /// <summary>
+    /// Методы на сервере уведомлений.
+    /// </summary>
+    public interface INotfyServerMethods
+    {
+        /// <summary>
+        /// Установить статус пользователя (вошел/вышел в программе).
+        /// </summary>
+        /// <param name="status">Статус.</param>
+        /// <returns></returns>
+        void SetStatus(bool status);
+
+        /// <summary>
+        /// Уведомить об изменении студента.
+        /// </summary>
+        /// <param name="oldGroupId">ИД группы, в которой находился студент.</param>
+        /// <param name="newGroupId">ИД группы, в котором находится студент.</param>
+        void ChangeStudent(int oldGroupId, int newGroupId);
+
+        /// <summary>
+        /// Уведомить об изменении группы.
+        /// </summary>
+        /// <param name="oldDivision">Подразделение, в котором находилась группа.</param>
+        /// <param name="newDivision">Подразделение, в котором находится группа.</param>
+        void ChangeGroup(int oldDivision, int newDivision);
+
+        /// <summary>
+        /// Уведомить об изменении сотрудника.
+        /// </summary>
+        void ChangeStaff();
+
+        /// <summary>
+        /// Уведомить об изменении пользователя.
+        /// </summary>
+        void ChangeUser();
+    }
+}
---
+++ Kursach.Core/INotifyServerEvents.cs
@@ -0,0 +1,32 @@
+﻿namespace Kursach.Core
+{
+    /// <summary>
+    /// События, которые происходят на сервере уведомлений.
+    /// </summary>
+    public interface INotifyServerEvents
+    {
+        /// <summary>
+        /// Уведомить об изменении студента.
+        /// </summary>
+        /// <param name="oldGroupId">ИД группы, в которой находился студент.</param>
+        /// <param name="newGroupId">ИД группы, в котором находится студент.</param>
+        void StudentChanged(int oldGroupId, int newGroupId);
+
+        /// <summary>
+        /// Уведомить об изменении группы.
+        /// </summary>
+        /// <param name="oldDivision">Подразделение, в котором находилась группа.</param>
+        /// <param name="newDivision">Подразделение, в котором находится группа.</param>
+        void GroupChanged(int oldDivision, int newDivision);
+
+        /// <summary>
+        /// Уведомить об изменении сотрудника.
+        /// </summary>
+        void StaffChanged();
+
+        /// <summary>
+        /// Уведомить об изменении пользователя.
+        /// </summary>
+        void UserChanged();
+    }
+}
