58bbb47286907577189f1a6cd75339dedfe4190b
Alexander <zam.kuratora@gmail.com>
2020-01-30 11:29:59 +0300

    cosmetic edits

--- Kursach/Views/MainWindow.xaml.cs
+++ Kursach/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Kursach.Properties;
+using Prism.Regions;
+using System.Windows;
 
 namespace Kursach
 {
@@ -7,9 +9,44 @@ namespace Kursach
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        /// <summary>
+        /// Менеджер регионов.
+        /// </summary>
+        readonly IRegionManager regionManager;
+
+        public MainWindow(IRegionManager regionManager)
         {
             InitializeComponent();
+
+            this.regionManager = regionManager;
+
+            var settings = Settings.Default;
+            Left = settings.left;
+            Top = settings.top;
+            Width = settings.width;
+            Height = settings.height;
+
+            if (settings.state == WindowState.Minimized)
+                settings.state = WindowState.Normal;
+
+            WindowState = settings.state;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var settings = Settings.Default;
+            settings.left = Left;
+            settings.top = Top;
+            settings.width = Width;
+            settings.height = Height;
+            settings.state = WindowState;
+
+            settings.Save();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            regionManager.RequestNavigateInRootRegion(RegionViews.LoginView);
         }
     }
 }
