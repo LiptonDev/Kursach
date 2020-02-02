52a3c83bf0a17837085670e9a9ed7913637c4621
Alexander <zam.kuratora@gmail.com>
2020-02-02 22:04:46 +0300

    new converter

---
+++ Kursach/Converters/BoolToValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Kursach.Converters
+{
+    /// <summary>
+    /// Конвертер bool -> object.
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(object))]
+    class BoolToValueConverter : IValueConverter
+    {
+        public object TrueValue { get; set; }
+        public object FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
