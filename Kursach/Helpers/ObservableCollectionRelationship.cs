2ae70e4df2a0a25392d41c76f8e6ce1063c6fcdb
Alexander <zam.kuratora@gmail.com>
2020-02-02 22:05:48 +0300

    added new extension for ObservableCollection

---
+++ Kursach/Helpers/ObservableCollectionRelationship.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kursach.Helpers
+{
+    /// <summary>
+    /// Установка связи одной ObservableCollection с другой.
+    /// </summary>
+    static class ObservableCollectionRelationship
+    {
+        /// <summary>
+        /// Установка связи одной ObservableCollection с другой.
+        /// </summary>
+        public static void SetRelationship<TSource, TRelation>(this ObservableCollection<TSource> sources,
+                                                                    ObservableCollection<TRelation> relations,
+                                                                    Func<TSource, TRelation, bool> predicate,
+                                                                    TaskFactory sync)
+        {
+            sources = sources ?? throw new ArgumentNullException(nameof(sources));
+            relations = relations ?? throw new ArgumentNullException(nameof(relations));
+            predicate = predicate ?? throw new ArgumentNullException(nameof(predicate));
+            sync = sync ?? throw new ArgumentNullException(nameof(sync));
+
+            sources.CollectionChanged += (sender, e) =>
+            {
+                if (e.Action != NotifyCollectionChangedAction.Remove)
+                    return;
+
+                TSource owner = (TSource)e.OldItems[0];
+                var forRemove = relations.Where(x => predicate(owner, x));
+                foreach (var item in forRemove)
+                {
+                    sync.StartNew(() => relations.Remove(item));
+                }
+            };
+        }
+    }
+}
