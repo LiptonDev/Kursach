61ea3db8a5a1624e0ae3d90d5ae05c07215a1720
Alexander <zam.kuratora@gmail.com>
2020-02-02 22:05:24 +0300

    added data provider

---
+++ Kursach/Providers/IDataProvider.cs
@@ -0,0 +1,41 @@
+﻿using Kursach.Core.Models;
+using System.Collections.ObjectModel;
+
+namespace Kursach.Providers
+{
+    /// <summary>
+    /// Поставщик данных.
+    /// </summary>
+    interface IDataProvider
+    {
+        /// <summary>
+        /// Пользователи.
+        /// </summary>
+        ObservableCollection<User> Users { get; }
+
+        /// <summary>
+        /// Сотрудники.
+        /// </summary>
+        ObservableCollection<Staff> Staff { get; }
+
+        /// <summary>
+        /// Группы.
+        /// </summary>
+        ObservableCollection<Group> Groups { get; }
+
+        /// <summary>
+        /// Студенты.
+        /// </summary>
+        ObservableCollection<Student> Students { get; }
+
+        /// <summary>
+        /// Загрузка данных.
+        /// </summary>
+        void Load(UserMode mode);
+
+        /// <summary>
+        /// Очистка данных.
+        /// </summary>
+        void Clear();
+    }
+}
