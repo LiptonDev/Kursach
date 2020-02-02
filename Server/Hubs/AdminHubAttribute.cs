f42a57f69fee3a4ddbb89d6e3e823325a15c470a
Alexander <zam.kuratora@gmail.com>
2020-02-02 22:02:15 +0300

    initial

---
+++ Server/Hubs/AdminHubAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Kursach.Core.Models;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Server.Hubs
+{
+    /// <summary>
+    /// Указывает, что доступ к хабу есть только у администраторов.
+    /// </summary>
+    class AdminHubAttribute : AuthorizeAttribute
+    {
+        public override bool AuthorizeHubMethodInvocation(IHubIncomingInvokerContext context, bool appliesToMethod)
+        {
+            LoginHub.Users.TryGetValue(context.Hub.Context.ConnectionId, out var mode);
+
+            return mode == UserMode.Admin;
+        }
+
+        public override bool AuthorizeHubConnection(HubDescriptor hubDescriptor, IRequest request)
+        {
+            return true;
+        }
+    }
+}
