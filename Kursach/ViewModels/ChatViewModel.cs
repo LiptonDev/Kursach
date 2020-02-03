fe1b276918ba32e85fe7f41f7b1eb3d65eafa061
Alexander <zam.kuratora@gmail.com>
2020-02-03 22:32:31 +0300

    added chat

---
+++ Kursach/ViewModels/ChatViewModel.cs
@@ -0,0 +1,71 @@
+﻿using DevExpress.Mvvm;
+using Kursach.Client.Interfaces;
+using Kursach.Models;
+using Kursach.Providers;
+using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Kursach.ViewModels
+{
+    /// <summary>
+    /// Chat view model.
+    /// </summary>
+    class ChatViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Текст сообщения.
+        /// </summary>
+        public string MessageText { get; set; }
+
+        /// <summary>
+        /// Сообщения.
+        /// </summary>
+        public ObservableCollection<ChatMessage> Messages { get; }
+
+        /// <summary>
+        /// Клиент сервера.
+        /// </summary>
+        readonly IClient client;
+
+        /// <summary>
+        /// Конструктор для DesignTime.
+        /// </summary>
+        public ChatViewModel()
+        {
+            Messages = new ObservableCollection<ChatMessage>
+            {
+                new ChatMessage(new Core.Models.User{ Login = "DESIGN NAME"}, "TEXT")
+            };
+        }
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public ChatViewModel(IDataProvider dataProvider, IClient client)
+        {
+            Messages = dataProvider.ChatMessages;
+            this.client = client;
+
+            SendMessageCommand = new DelegateCommand(SendMessage);
+        }
+
+        /// <summary>
+        /// Команда отправки сообщения.
+        /// </summary>
+        public ICommand SendMessageCommand { get; }
+
+        /// <summary>
+        /// Отправить сообщение.
+        /// </summary>
+        private void SendMessage()
+        {
+            if (MessageText.IsEmpty())
+                return;
+
+            client.Chat.SendMessage(MessageText);
+
+            MessageText = "";
+        }
+    }
+}
