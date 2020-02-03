d33084e08995bd79369c55dfc6b2586041f6fef2
Alexander <zam.kuratora@gmail.com>
2020-02-03 22:32:06 +0300

    added chat

---
+++ Kursach/Models/ChatMessage.cs
@@ -0,0 +1,36 @@
+﻿using Kursach.Core.Models;
+using System;
+
+namespace Kursach.Models
+{
+    /// <summary>
+    /// Сообщение в чате.
+    /// </summary>
+    class ChatMessage
+    {
+        /// <summary>
+        /// Отправитель.
+        /// </summary>
+        public User User { get; }
+
+        /// <summary>
+        /// Текст сообщения.
+        /// </summary>
+        public string Text { get; }
+
+        /// <summary>
+        /// Время получения сообщения.
+        /// </summary>
+        public string Time { get; }
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public ChatMessage(User user, string text)
+        {
+            User = user;
+            Text = text;
+            Time = DateTime.Now.ToShortTimeString();
+        }
+    }
+}
